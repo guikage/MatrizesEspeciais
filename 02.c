1bdd0adccef081a11e83161895f53063a2720b90
Jaime Filho <jaimeadanielfilho@hotmail.com>
2023-09-06 15:13:10 -0300

    exercicio 3

--- 02.c
+++ 02.c
@@ -20,7 +20,7 @@ struct esparsa
 
 typedef struct esparsa Esparsa;
 
-Esparsa criar_matriz_esparsa(int linhas, int colunas) {
+Esparsa matriz_esparsa_criar(int linhas, int colunas) {
     return (Esparsa) {
         .linhas = linhas,
         .colunas = colunas,
@@ -28,7 +28,7 @@ Esparsa criar_matriz_esparsa(int linhas, int colunas) {
     };
 }
 
-int quantidade_preenchida_matriz_esparsa(Esparsa *esparsa) {
+int matriz_esparsa_quantidade_de_elementos(Esparsa *esparsa) {
     int tamanho = 0;
     
     for (Lista *no = esparsa->prim; no != NULL; no = no->prox) {
@@ -38,14 +38,14 @@ int quantidade_preenchida_matriz_esparsa(Esparsa *esparsa) {
     return tamanho;
 }
 
-double percentual_preenchido_matriz_esparsa(Esparsa *esparsa) {
-    int preenchido = quantidade_preenchida_matriz_esparsa(esparsa);
+double matriz_esparsa_preenchido(Esparsa *esparsa) {
+    int preenchido = matriz_esparsa_quantidade_de_elementos(esparsa);
     int total = esparsa->linhas * esparsa->colunas;
 
     return (double)preenchido / total;
 }
 
-void inserir_elemento_matriz_esparsa(Esparsa *esparsa, int linha, int coluna, int info) {
+void matriz_esparsa_inserir_elemento(Esparsa *esparsa, int linha, int coluna, int info) {
     Lista *no = (Lista *)malloc(sizeof(Lista));
 
     if (no == NULL) {
@@ -61,7 +61,7 @@ void inserir_elemento_matriz_esparsa(Esparsa *esparsa, int linha, int coluna, in
     esparsa->prim = no;
 }
 
-int consultar_elemento_matriz_esparsa(Esparsa *esparsa, int linha, int coluna) {
+int matriz_esparsa_consultar_elemento(Esparsa *esparsa, int linha, int coluna) {
     for (Lista *no = esparsa->prim; no != NULL; no = no->prox) {
         if (no->linha == linha && no->coluna == coluna) {
             return no->info;
@@ -71,7 +71,7 @@ int consultar_elemento_matriz_esparsa(Esparsa *esparsa, int linha, int coluna) {
     return 0;
 }
 
-int somar_linha_matriz_esparsa(Esparsa *esparsa, int linha) {
+int matriz_esparsa_somar_linha(Esparsa *esparsa, int linha) {
     int soma = 0;
 
     for (Lista *no = esparsa->prim; no != NULL; no = no->prox) {
@@ -83,12 +83,12 @@ int somar_linha_matriz_esparsa(Esparsa *esparsa, int linha) {
     return soma;
 }
 
-void preencher_matriz_esparsa(Esparsa *esparsa) {
+void matriz_esparsa_preencher(Esparsa *esparsa) {
     int linha, coluna;
 
     do {
 
-        printf("Em qual linha e em qual coluna voce deseja inserir (-1 -1 para finalizar)? ");
+        printf("Digite a linha e a coluna (-1 -1 para finalizar): ");
         scanf("%d %d", &linha, &coluna);
 
         if (linha > -1 && coluna > -1) {
@@ -98,7 +98,7 @@ void preencher_matriz_esparsa(Esparsa *esparsa) {
             scanf("%d", &info);
 
             if (info != 0) {
-                inserir_elemento_matriz_esparsa(esparsa, linha, coluna, info);
+                matriz_esparsa_inserir_elemento(esparsa, linha, coluna, info);
             }
         }
         
@@ -106,7 +106,7 @@ void preencher_matriz_esparsa(Esparsa *esparsa) {
     } while (linha > -1 && coluna > -1);
 }
 
-void imprimir_matriz_esparsa(Esparsa *esparsa) {
+void matriz_esparsa_imprimir(Esparsa *esparsa) {
     printf("   ");
 
     for (int j = 0; j < esparsa->colunas; j++) {
@@ -119,7 +119,7 @@ void imprimir_matriz_esparsa(Esparsa *esparsa) {
         printf("%03d", i);
 
         for (int j = 0; j < esparsa->colunas; j++) {
-            int elemento = consultar_elemento_matriz_esparsa(esparsa, i, j);
+            int elemento = matriz_esparsa_consultar_elemento(esparsa, i, j);
             printf(" %3d", elemento);
         }
 
@@ -139,6 +139,45 @@ void imprimir_menu() {
     printf("s) Sair\n\n");
 }
 
+void escolher_opcao(Esparsa *esparsa, char opcao) {
+    switch (opcao) {
+        case 'a':
+            matriz_esparsa_imprimir(esparsa);
+            break;
+        case 'b':
+            matriz_esparsa_preencher(esparsa);
+            break;
+        case 'c':
+            {
+                int linha, coluna;
+                printf("Digite a linha e a coluna para consultar: ");
+                scanf("%d %d", &linha, &coluna);
+
+                int elemento = matriz_esparsa_consultar_elemento(esparsa, linha, coluna);
+
+                printf("[%d, %d] = %d\n\n", linha, coluna, elemento);
+                break;
+            }
+        case 'd':
+            {
+                int linha;
+                printf("Digite a linha para somar: ");
+                scanf("%d", &linha);
+
+                printf("Soma: %d\n\n", matriz_esparsa_somar_linha(esparsa, linha));
+
+                break;
+            }
+        case 'i':
+            {
+                double porcentagem = matriz_esparsa_preenchido(esparsa);
+                printf("Total preenchido: %.2lf%%\n\n", porcentagem * 100);
+
+                break;
+            }
+    }
+}
+
 int main() {
     int linhas, colunas;
     
@@ -148,7 +187,7 @@ int main() {
     printf("Digite o numero de colunas da matriz: ");
     scanf("%d", &colunas);
 
-    Esparsa esparsa = criar_matriz_esparsa(linhas, colunas);
+    Esparsa esparsa = matriz_esparsa_criar(linhas, colunas);
 
     char opcao;
 
@@ -158,44 +197,7 @@ int main() {
         printf("O que voce deseja fazer? ");
         scanf(" %c", &opcao);
 
-        switch (opcao)
-        {
-            case 'a':
-                imprimir_matriz_esparsa(&esparsa);
-                break;
-            case 'b':
-                preencher_matriz_esparsa(&esparsa);
-                break;
-            case 'c':
-                {
-                    int linha, coluna;
-                    printf("Digite a linha e a coluna para consultar: ");
-                    scanf("%d %d", &linha, &coluna);
-
-                    int elemento = consultar_elemento_matriz_esparsa(&esparsa, linha, coluna);
-
-                    printf("[%d, %d] = %d\n\n", linha, coluna, elemento);
-                    break;
-                }
-            case 'd':
-                {
-                    int linha;
-                    printf("Digite a linha para somar: ");
-                    scanf("%d", &linha);
-
-                    printf("Soma: %d\n\n", somar_linha_matriz_esparsa(&esparsa, linha));
-
-                    break;
-                }
-            case 'i':
-                {
-                    double porcentagem = percentual_preenchido_matriz_esparsa(&esparsa);
-                    printf("Total preenchido: %.2lf%%\n\n", porcentagem * 100);
-
-                    break;
-                }
-
-        }
+        escolher_opcao(&esparsa, opcao);        
     } while (opcao != 's');
 
 
